a7f123b4eb488cb0fe326217ee99dbc89abb15be
Hernan Pentimalli <hpentimalli@gmail.com>
2020-08-25 16:26:21 -0300

    Added DecodeError to public exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { fromJson } from './decodeJson';
+
 export * from './decodeJson';
 export * from './baseDecoders';
 export * from './decoder';
 export * from './decodeObject';
 export * from './decodeArrayOptions';
 export * from './decodeArray';
+export * from './decodeError';
